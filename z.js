a62d462be9918d7556e647b52ddf5386d5b64908
hanzhecheng <1119216543@qq.com>
2019-04-03 14:54:53 +0800

    no message

--- z.js
+++ z.js
@@ -61,3 +61,46 @@ two.forEach((item, index) => {
     }
 })
 console.log(JSON.stringify(two))
+
+
+function xxx() {
+    var that = this
+    var currentPage = 0; // 因为数组下标是从0开始的，所以这里用了0
+    var tips = "加载第" + (currentPage + 1) + "页";
+    console.log("load page " + (currentPage + 1));
+    wx.showLoading({
+        title: tips,
+    }) // 刷新时，清空dataArray，防止新数据与原数据冲突
+    that.setData({
+        dataArray: []
+    }) // 请封装自己的网络请求接口，这里作为示例就直接使用了wx.request.
+    wx.request({
+        url: 'https://raw.githubusercontent.com/lanfeng1993/LoadDataDemo/master/data/data.json',
+        data: {},
+        header: {
+            'content-type': 'application/json'
+        },
+        success: function (res) {
+            wx.hideLoading();
+            var data = res.data; // 接口相应的json数据
+            var articles = data.data; // 接口中的data对应了一个数组，这里取名为 articles
+            var totalDataCount = articles.length; // console.log(articles);
+            console.log("totalDataCount:" + totalDataCount);
+            that.setData({
+                ["dataArray[" + currentPage + "]"]: articles,
+                currentPage: currentPage,
+                totalDataCount: totalDataCount
+            })
+        }
+    })
+
+}
+<scroll-view scroll-y="true" style = "height:100%;position: relative;" scroll-into-view="{{toView}}" scroll-with-animation = "true">
+    <view id = "top"></view>
+    <block wx:for="{{dataArray}}" wx:for-item="articles" wx:for-index="dataArrayIndex">
+      <block wx:for="{{articles}}" wx:for-item="item" wx:key="{{item.id}}" wx:for-index="index">
+        <template is = "articles" data='{{item:item,index:index,dataArrayIndex:dataArrayIndex}}'/> <!--将item和index都传到模版里去，不然模版中获取不到index-->
+      </block>
+    </block>
+    <view id = "bottom"></view>
+  </scroll-view>
